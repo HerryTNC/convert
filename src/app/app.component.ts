1d63ec88f5180529286ccc6b2b6ed921c7694397
unknown <herry220388@gmail.com>
2018-06-12 11:34:09 +0700

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Date Converter';
+
+  public ngOnInit()
+  {
+    $(document).ready(function(){
+        //var input_tgl = $('#isoDate').val();
+		let today = moment($('#isoDate').val(),'MM/DD/YYYY HH:mm').toISOString();
+		let formatted = moment(today, moment.ISO_8601).format('MM/DD/YYYY HH:mm');
+		
+		$('.convert').on('click', function() {
+			console.log(moment($('#isoDate').val(),'MM/DD/YYYY HH:mm'));
+			formatted = moment(today, moment.ISO_8601).format('ddd, MMM Do HH:MMa');
+			$('#dateformat').append(formatted);
+		});
+    });
+  }
 }
